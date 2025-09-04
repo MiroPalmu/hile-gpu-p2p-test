77c87bec567f210dad3ae96c734d2ebdbe93590a
Palmu Miro <miro.palmu@helsinki.fi>
2025-09-04 10:32:41 +0300

    tidy up env

--- test_env.sh
+++ test_env.sh
@@ -3,10 +3,4 @@ module load cpe/24.11
 
 # After cpe we don't specify versions as cpe module makes defaults be sensible.
 module load PrgEnv-cray craype-accel-amd-gfx90a cray-mpich libfabric rocm cray-python cray-pmi
-# module load PrgEnv-amd craype-accel-amd-gfx90a craype-network-ucx libfabric rocm cray-python cray-pmi
-# module swap cray-mpich cray-mpich-ucx
-
-
 module list
-
-export PYTHONPATH="$PYTHONPATH:/wrk-vakka/users/pamiro/hip_tests/build"
