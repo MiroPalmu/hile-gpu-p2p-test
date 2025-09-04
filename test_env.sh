1d35b594f2d6aab591572b5f887bc2b321a7e532
Miro Palmu <email@miropalmu.cc>
2025-09-04 11:18:23 +0300

    tidy test env more

--- test_env.sh
+++ test_env.sh
@@ -2,5 +2,5 @@ module purge
 module load cpe/24.11
 
 # After cpe we don't specify versions as cpe module makes defaults be sensible.
-module load PrgEnv-cray craype-accel-amd-gfx90a cray-mpich libfabric rocm cray-python cray-pmi
+module load PrgEnv-cray craype-accel-amd-gfx90a cray-mpich libfabric
 module list
